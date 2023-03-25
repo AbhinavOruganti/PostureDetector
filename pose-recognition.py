d6c6bdf587f93e43b58d2315affcf6d0504eb852
Shivam Katyan <shivam.katyan@nagarro.com>
2023-03-25 19:36:33 +0530

    added more landmarks in calculation for slouching

--- pose-recognition.py
+++ pose-recognition.py
@@ -4,7 +4,7 @@ from win10toast import ToastNotifier
 import time
 
 mpPoseInstance = mp.solutions.pose
-pose = mpPoseInstance.Pose()
+pose = mpPoseInstance.Pose(static_image_mode=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 mpDrawUtility = mp.solutions.drawing_utils
 toaster = ToastNotifier()
 
@@ -24,13 +24,22 @@ while True:
             right_shoulder = results.pose_landmarks.landmark[mpPoseInstance.PoseLandmark.RIGHT_SHOULDER]
             left_hip = results.pose_landmarks.landmark[mpPoseInstance.PoseLandmark.LEFT_HIP]
             right_hip = results.pose_landmarks.landmark[mpPoseInstance.PoseLandmark.RIGHT_HIP]
+            left_elbow = results.pose_landmarks.landmark[mpPoseInstance.PoseLandmark.LEFT_ELBOW]
+            right_elbow = results.pose_landmarks.landmark[mpPoseInstance.PoseLandmark.RIGHT_ELBOW]
+            left_wrist = results.pose_landmarks.landmark[mpPoseInstance.PoseLandmark.LEFT_WRIST]
+            right_wrist = results.pose_landmarks.landmark[mpPoseInstance.PoseLandmark.RIGHT_WRIST]
+            mid_hip = results.pose_landmarks.landmark[mpPoseInstance.PoseLandmark.MID_HIP]
 
             # Calculate shoulder and hip widths
             shoulder_width = abs(left_shoulder.x - right_shoulder.x) * source.shape[1]
             hip_width = abs(left_hip.x - right_hip.x) * source.shape[1]
 
+            # Calculate angle between shoulders and hips
+            shoulder_hip_angle = abs(mp.solutions.angle_calculator.get_angle(
+                left_shoulder, mid_hip, right_shoulder).degrees)
+
             # Check if posture is incorrect
-            if shoulder_width > hip_width:
+            if shoulder_width > hip_width and shoulder_hip_angle < 170:
                 if correct_posture:
                     # Incorrect posture detected
                     start_time = time.time()
@@ -47,11 +56,18 @@ while True:
 
             # Draw landmarks on frame
             mpDrawUtility.draw_landmarks(source, results.pose_landmarks, mpPoseInstance.POSE_CONNECTIONS)
-            for id, lm in enumerate(results.pose_landmarks.landmark):     
+
+            # Draw circles on the shoulder, elbow, and wrist landmarks
+            for id, lm in enumerate(results.pose_landmarks.landmark):
                 h, w, c = source.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
-                cv.circle(source, (cx, cy), 5, (255,0,0), cv.FILLED)
-
+                if id == mpPoseInstance.PoseLandmark.LEFT_SHOULDER.value or id == mpPoseInstance.PoseLandmark.RIGHT_SHOULDER.value:
+                    cv.circle(source, (cx, cy), 10, (0, 255, 255), cv.FILLED)
+                elif id == mpPoseInstance.PoseLandmark.LEFT_ELBOW.value or id == mpPoseInstance.PoseLandmark.RIGHT_ELBOW.value:
+                    cv.circle(source, (cx, cy), 10, (255, 255, 0), cv.FILLED)
+                elif id == mpPoseInstance.PoseLandmark.LEFT_WRIST.value or id == mpPoseInstance.PoseLandmark.RIGHT_WRIST.value:
+                    cv.circle(source, (cx, cy), 10, (255, 0, 255), cv.FILLED)
+                
         # Display the resulting frame
         cv.imshow('Frame', source)
 
@@ -65,3 +81,6 @@ while True:
 # When everything done, release the capture
 capture.release()
 cv.destroyAllWindows()
+
+            
+            
