612ceee120465265133022ab1caf3793c5bd8135
Abhinav Oruganti <orugantiabhinav5@gmail.com>
2023-03-25 11:51:52 +0530

    fixed frames issue with video

--- pose-recognition.py
+++ pose-recognition.py
@@ -1,32 +1,24 @@
 import cv2 as cv
 import mediapipe as mp
 
-mpPoseInstance = mp.solutions.pose
-pose = mpPoseInstance.Pose()
-mpDrawUtility = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+mpDraw = mp.solutions.drawing_utils
 
-# capture = cv.VideoCapture('assets/sample-clip.mp4')
-capture = cv.VideoCapture('assets/human1.jpg')
+cap = cv.VideoCapture('assets/sample-clip.mp4')
 
 while True:
-    success, source = capture.read()
+    success, source = cap.read()
+    imgRGB = cv.cvtColor(source, cv.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results.pose_landmarks)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(source, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w,c = source.shape
+            print(id, lm)
+            cx, cy = int(lm.x*w), int(lm.y*h)
+            cv.circle(source, (cx, cy), 5, (255,0,0), cv.FILLED)
 
-    if source is not None:
-        # imgRGB = cv.cvtColor(source, cv.COLOR_BGR2RGB)
-        results = pose.process(source) 
-        # pose.process(imgRGB)
-        # print(results.pose_landmarks)
-        if results.pose_landmarks:
-            mpDrawUtility.draw_landmarks(source, results.pose_landmarks, mpPoseInstance.POSE_CONNECTIONS)
-            for id, lm in enumerate(results.pose_landmarks.landmark):     
-                h, w, c = source.shape
-                # print(id, lm)
-                cx, cy = int(lm.x*w), int(lm.y*h)
-                cv.circle(source, (cx, cy), 5, (255,0,0), cv.FILLED)
-
-        cv.imshow("Capture", source)
-
-        # to apply some delay
-        # cv.waitKey(100000)
-    else:
-        cv.destroyAllWindows()
+    cv.imshow("Source", source)
+    cv.waitKey(1)
